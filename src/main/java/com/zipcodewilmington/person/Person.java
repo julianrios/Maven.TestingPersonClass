fd741ac00b7a5779c883060de38d2abaa5bf83fd
Julian Rios <julianrios@zipcoders-MacBook-Pro-3.local>
2019-02-14 15:57:20 -0500

    added passing test code for person class constructors setters and getters

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.person;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 /**
  * Created by leon on 2/12/18.
  */
@@ -7,29 +9,91 @@ public class Person {
     private String name;
     private int age;
 
+    private String gender;
+    private String eyeColor;
+    private String hairColor;
+    private Integer birthYear;
+    private Boolean isAdult;
+
     public Person() {
     }
 
-    public Person(int age) {
+    public Person(String name) {
+        this.name = name;
     }
 
-    public Person(String name) {
+    public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
-    public void setName(String name) {
-    }
-
-    public void setAge(int age) {
+    public Person(String name, int age, String gender, String eyeColor, String hairColor, Integer birthYear, Boolean isAdult) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.eyeColor = eyeColor;
+        this.hairColor = hairColor;
+        this.birthYear = birthYear;
+        this.isAdult = isAdult;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
-    public Integer getAge() {
-        return null;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public void setEyeColor(String eyeColor) {
+        this.eyeColor = eyeColor;
+    }
+
+    public String getHairColor() {
+        return hairColor;
+    }
+
+    public void setHairColor(String hairColor) {
+        this.hairColor = hairColor;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+
+    public boolean getIsAdult() {
+        return isAdult;
+    }
+
+    public void setAdult(boolean adult) {
+        isAdult = adult;
     }
 }
--- src/test/java/com/zipcodewilmington/person/TestPerson.java
+++ src/test/java/com/zipcodewilmington/person/TestPerson.java
@@ -7,21 +7,15 @@ import org.junit.Test;
  * Created by leon on 2/12/18.
  */
 public class TestPerson {
+
     @Test
     public void testDefaultConstructor() {
-        // Given
-        String expectedName = "";
-        Integer expectedAge = Integer.MAX_VALUE;
-
         // When
         Person person = new Person();
 
         // Then
-        String actualName = person.getName();
-        Integer actualAge = person.getAge();
-
-        Assert.assertEquals(expectedName, actualName);
-        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertNull(person.getName());
+        Assert.assertEquals(0, person.getAge());
     }
 
     @Test
@@ -69,6 +63,41 @@ public class TestPerson {
     }
 
     @Test
+    public void testConstructorWithAllFields() {
+        // Given
+        Integer expectedAge = 5;
+        String expectedName = "Julian";
+        String expectedGender = "male";
+        String expectedEyeColor = "brown";
+        String expectedHairColor = "brown";
+        Integer expectedBirthYear = 1990;
+        Boolean expectedIsAdult = true;
+
+
+        // When
+        Person person = new Person(expectedName, expectedAge, expectedGender, expectedEyeColor,expectedHairColor,
+                expectedBirthYear,expectedIsAdult);
+
+        // Then
+        Integer actualAge = person.getAge();
+        String actualName = person.getName();
+        String actualGender = person.getGender();
+        String actualEyeColor = person.getEyeColor();
+        String actualHairColor = person.getHairColor();
+        Integer actualBirthYear = person.getBirthYear();
+        Boolean actualIsAdult = person.getIsAdult();
+
+
+        Assert.assertEquals(expectedAge, actualAge);
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedGender, actualGender);
+        Assert.assertEquals(expectedEyeColor, actualEyeColor);
+        Assert.assertEquals(expectedHairColor, actualHairColor);
+        Assert.assertEquals(expectedBirthYear, actualBirthYear);
+        Assert.assertEquals(expectedIsAdult, actualIsAdult);
+    }
+
+    @Test
     public void testSetName() {
         // Given
         Person person = new Person();
@@ -95,4 +124,137 @@ public class TestPerson {
         Integer actual = person.getAge();
         Assert.assertEquals(expected, actual);
     }
+
+    @Test
+    public void setEyeColorTest() {
+        // Given
+        Person person = new Person();
+        String expected = "brown" ;
+
+        // When
+        person.setEyeColor(expected);
+
+        // Then
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getEyeColorTest() {
+        // Given
+        String expected = "blue" ;
+        Person person = new Person(null,0, null, expected,null,
+                null, null);
+
+        // Then
+        String actual = person.getEyeColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setGenderTest() {
+        // Given
+        Person person = new Person();
+        String expected = "male" ;
+
+        // When
+        person.setGender(expected);
+
+        // Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getGenderTest() {
+        // Given
+        String expected = "male" ;
+        Person person = new Person(null,0,expected,null,null,
+                null, null);
+
+
+        // Then
+        String actual = person.getGender();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setHairColorTest() {
+        // Given
+        String expected = "black" ;
+        Person person = new Person();
+
+        // When
+        person.setHairColor(expected);
+
+        // Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getHairColorTest() {
+        // Given
+        String expected = "black" ;
+        Person person = new Person(null,0,null,null,expected,
+                null, null);
+
+        // Then
+        String actual = person.getHairColor();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setBirthYearTest() {
+        // Given
+        Integer expected = 1990;
+        Person person = new Person();
+
+        // When
+        person.setBirthYear(expected);
+
+        // Then
+        Integer actual = person.getBirthYear();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getBirthYearTest() {
+        // Given
+        Integer expected = 2019;
+        Person person = new Person(null,0,null,null,null,
+                expected, null);
+
+        // Then
+        Integer actual = person.getBirthYear();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setAdultTest() {
+        // Given
+        Boolean expected = true;
+        Person person = new Person();
+
+        // When
+        person.setAdult(expected);
+
+        // Then
+        Boolean actual = person.getIsAdult();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getIsAdultTest() {
+        // Given
+        Boolean expected = false;
+        Person person = new Person(null,0,null,null,null,
+                null, expected);
+
+        // Then
+        Boolean actual = person.getIsAdult();
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
